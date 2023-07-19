046aa271c0b1d83e16b458ef931e2174f93311f8
AmroSous <amroosous@gmail.com>
2023-07-19 12:09:07 +0300

    write hill climbing and simulated annealing algorithms

--- src/nqueen/Application.java
+++ src/nqueen/Application.java
@@ -5,59 +5,161 @@
 package nqueen;
 
 import java.awt.*;
-import java.awt.event.*;
 import java.util.HashMap;
 import java.util.stream.Stream;
 import javax.swing.*;
-import javax.swing.event.*;
+import javax.swing.border.*;
 
 /**
- * @author amroo
+ *    This is the main Application class which extends JFrame class
+ *    and contains control panels and the chess board.
  */
+@SuppressWarnings("FieldCanBeLocal")
 public class Application extends JFrame {
-    
-    private Board board;
+
+    /**
+     * board components of class Board, is the main component in the application
+     * which contains the algorithms implementation for solving the problem and
+     * it paints the chess board on the application frame
+     */
+    private final Board board;
     public Application() {
         
         initComponents();
         
         this.setTitle("N-Queen Solver");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        board = new Board();
+        board = new Board(this);
         chessBoardPanel.add(board);
-        board.setBounds(15, 15, chessBoardPanel.getWidth() - 30, chessBoardPanel.getHeight() - 30);
+        board.setBounds(5, 5, chessBoardPanel.getWidth() - 10, chessBoardPanel.getHeight() - 10);
         board.setBackground(chessBoardPanel.getBackground());
     }
 
+    /**
+     * setter to access the iteration field in the result panel
+     * used by the board component to set the final iteration of algorithm.
+     */
+    public void setIterationField(int value) {
+        iterationField.setText(value+"");
+    }
+
+    /**
+     * setter to access the temperature field in the result panel
+     * used by the board component to set the final temperature after
+     * applying simulated annealing algorithm.
+     */
+    public void setTemperatureField(double value) {
+        temperatureField.setText(value+"");
+    }
+
+    /**
+     * to make the panel which takes the parameters of simulated annealing algorithm enabled.
+     */
     private void setAnnealingPanelEnable(boolean state) {
         Stream.of(simAnnealingPanel.getComponents()).forEach(c -> c.setEnabled(state));
     }
 
-    private void algorithmComboItemStateChanged(ItemEvent e) {
+    /**
+     * state changed listener for algorithm combo to enable sim-ann panel
+     */
+    private void algorithmComboItemStateChanged() {
         setAnnealingPanelEnable(algorithmCombo.getSelectedIndex() != 0);
     }
 
-    private void initialize(ActionEvent e) {
+    /**
+     * action listener for random button
+     */
+    private void initialize() {
         board.initializeState();
     }
 
-    private void run(ActionEvent e) {
+    /**
+     * action listener for run button, when pressed the method takes all inputs
+     * from the control panel and make validation on it, then pass it to the
+     * board component to run the algorithm
+     */
+    private void run() {
+        messageLabel.setText("");
+        String algorithm = (String) algorithmCombo.getSelectedItem();
+        String initialT = initialTemp.getText().trim();
+        String cooling = coolingRate.getText().trim();
+        String finalT = finalTemp.getText().trim();
+
+        String error = "";
+        if (algorithm == null || algorithm.equals("")) {
+            error = "Invalid Algorithm.";
+        }
+        else if (algorithm.equals("Simulated Annealing")) {
+            String pattern = "^[0-9]{1,9}(.([0-9]{1,5}))?$";
+            if (!initialT.matches(pattern) || !cooling.matches(pattern) || !finalT.matches(pattern)) {
+                error = "Invalid inputs in fields.";
+            }
+        }
+
+        if (!error.equals("")) {
+            messageLabel.setText(error);
+            return;
+        }
+
         HashMap<String, String> data = new HashMap<>();
-        data.put("algorithm", (String) algorithmCombo.getSelectedItem());
-        data.put("initial_temp", initialTemp.getText());
-        data.put("cooling_rate", coolingRate.getText());
-        data.put("final_temp", finalTemp.getText());
+        data.put("algorithm", algorithm);
+        data.put("initial_temp", initialT);
+        data.put("cooling_rate", cooling);
+        data.put("final_temp", finalT);
         board.runAlgorithm(data);
     }
 
-    private void createUIComponents() {
-        // TODO: add custom component creation code here
-    }
-
-    private void boardSizeSpinnerStateChanged(ChangeEvent e) {
+    /**
+     * listener for the spinner to change the board size
+     */
+    private void boardSizeSpinnerStateChanged() {
         board.setBoardSize((int)boardSizeSpinner.getValue());
     }
-    
+
+    /**
+     * listener to clear button which clears all the inputs and message label
+     */
+    private void clear() {
+        messageLabel.setText("");
+        initialTemp.setText("");
+        coolingRate.setText("");
+        finalTemp.setText("");
+    }
+
+    /**
+     * method to change theme to Light Theme
+     */
+    private void putLightTheme() {
+        controlPanel.setBackground(Color.decode("#f2f2f2"));
+        resultPanel.setBackground(Color.decode("#f2f2f2"));
+        Stream.of(resultPanel.getComponents()).filter(c -> c instanceof JLabel).forEach(l -> l.setForeground(Color.BLACK));
+        Stream.of(controlPanel.getComponents()).filter(c -> c instanceof JLabel).forEach(l -> l.setForeground(Color.BLACK));
+    }
+
+    /**
+     * method to change theme to Dark Theme
+     */
+    private void putDarkTheme() {
+        controlPanel.setBackground(Color.black);
+        resultPanel.setBackground(Color.black);
+        Stream.of(resultPanel.getComponents()).filter(c -> c instanceof JLabel).forEach(l -> l.setForeground(Color.cyan));
+        Stream.of(controlPanel.getComponents()).filter(c -> c instanceof JLabel).forEach(l -> l.setForeground(Color.cyan));
+    }
+
+    /**
+     * listener for light theme menu item
+     */
+    private void lightTheme() {
+        putLightTheme();
+    }
+
+    /**
+     * listener for dark theme menu item
+     */
+    private void darkTheme() {
+        putDarkTheme();
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Amro
@@ -79,6 +181,16 @@ public class Application extends JFrame {
         initializeButton = new JButton();
         runButton = new JButton();
         messageLabel = new JLabel();
+        clearButton = new JButton();
+        menuBar1 = new JMenuBar();
+        menu1 = new JMenu();
+        darkTheme = new JMenuItem();
+        lightTheme = new JMenuItem();
+        resultPanel = new JPanel();
+        label7 = new JLabel();
+        iterationField = new JTextField();
+        label8 = new JLabel();
+        temperatureField = new JTextField();
 
         //======== this ========
         setBackground(new Color(0xcccccc));
@@ -87,12 +199,12 @@ public class Application extends JFrame {
         //======== panel1 ========
         {
             panel1.setBackground(new Color(0xcccccc));
-            panel1.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border
-            .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax. swing .border . TitledBorder. CENTER ,javax
-            . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "D\u0069alog", java .awt . Font. BOLD ,
-            12 ) ,java . awt. Color .red ) ,panel1. getBorder () ) ); panel1. addPropertyChangeListener( new java. beans
-            .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062order" .equals ( e.
-            getPropertyName () ) )throw new RuntimeException( ) ;} } );
+            panel1.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing.
+            border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JFor\u006dDesi\u0067ner \u0045valu\u0061tion" , javax. swing .border . TitledBorder. CENTER
+            ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font
+            . BOLD ,12 ) ,java . awt. Color .red ) ,panel1. getBorder () ) ); panel1. addPropertyChangeListener(
+            new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "bord\u0065r"
+            .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
             panel1.setLayout(null);
 
             //======== chessBoardPanel ========
@@ -116,7 +228,7 @@ public class Application extends JFrame {
                 }
             }
             panel1.add(chessBoardPanel);
-            chessBoardPanel.setBounds(10, 6, 480, 475);
+            chessBoardPanel.setBounds(10, 6, 465, 409);
 
             //======== controlPanel ========
             {
@@ -126,7 +238,7 @@ public class Application extends JFrame {
                 label1.setText("Board Size: ");
                 label1.setFont(new Font("SimSun", Font.PLAIN, 18));
                 controlPanel.add(label1);
-                label1.setBounds(35, 70, 115, 40);
+                label1.setBounds(30, 90, 115, 40);
 
                 //---- label2 ----
                 label2.setText("Control Panel");
@@ -138,15 +250,15 @@ public class Application extends JFrame {
                 //---- boardSizeSpinner ----
                 boardSizeSpinner.setModel(new SpinnerNumberModel(8, 4, 8, 1));
                 boardSizeSpinner.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-                boardSizeSpinner.addChangeListener(e -> boardSizeSpinnerStateChanged(e));
+                boardSizeSpinner.addChangeListener(e -> boardSizeSpinnerStateChanged());
                 controlPanel.add(boardSizeSpinner);
-                boardSizeSpinner.setBounds(160, 75, 90, 32);
+                boardSizeSpinner.setBounds(155, 95, 90, 32);
 
                 //---- label3 ----
                 label3.setText("Algorithm:");
                 label3.setFont(new Font("SimSun", Font.PLAIN, 18));
                 controlPanel.add(label3);
-                label3.setBounds(35, 120, 115, 40);
+                label3.setBounds(30, 150, 115, 40);
 
                 //---- algorithmCombo ----
                 algorithmCombo.setModel(new DefaultComboBoxModel<>(new String[] {
@@ -154,9 +266,9 @@ public class Application extends JFrame {
                     "Simulated Annealing"
                 }));
                 algorithmCombo.setFont(new Font("SimSun", Font.PLAIN, 16));
-                algorithmCombo.addItemListener(e -> algorithmComboItemStateChanged(e));
+                algorithmCombo.addItemListener(e -> algorithmComboItemStateChanged());
                 controlPanel.add(algorithmCombo);
-                algorithmCombo.setBounds(160, 125, 180, 35);
+                algorithmCombo.setBounds(160, 155, 180, 35);
 
                 //======== simAnnealingPanel ========
                 {
@@ -221,28 +333,59 @@ public class Application extends JFrame {
                     }
                 }
                 controlPanel.add(simAnnealingPanel);
-                simAnnealingPanel.setBounds(35, 180, 345, 150);
+                simAnnealingPanel.setBounds(25, 210, 345, 150);
 
                 //---- initializeButton ----
                 initializeButton.setText("Initialize");
                 initializeButton.setFont(new Font("Sitka Text", Font.PLAIN, 16));
-                initializeButton.addActionListener(e -> initialize(e));
+                initializeButton.addActionListener(e -> initialize());
                 controlPanel.add(initializeButton);
-                initializeButton.setBounds(60, 360, 120, 40);
+                initializeButton.setBounds(135, 385, 105, 40);
 
                 //---- runButton ----
                 runButton.setText("Run");
                 runButton.setFont(new Font("Sitka Text", Font.PLAIN, 16));
-                runButton.addActionListener(e -> run(e));
+                runButton.addActionListener(e -> run());
                 controlPanel.add(runButton);
-                runButton.setBounds(225, 360, 120, 40);
+                runButton.setBounds(265, 385, 95, 40);
 
                 //---- messageLabel ----
                 messageLabel.setForeground(Color.red);
                 messageLabel.setFont(new Font("SimSun", Font.PLAIN, 16));
                 messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
                 controlPanel.add(messageLabel);
-                messageLabel.setBounds(40, 430, 335, 30);
+                messageLabel.setBounds(25, 450, 350, 25);
+
+                //---- clearButton ----
+                clearButton.setText("Clear");
+                clearButton.setFont(new Font("Sitka Text", Font.PLAIN, 16));
+                clearButton.addActionListener(e -> clear());
+                controlPanel.add(clearButton);
+                clearButton.setBounds(25, 385, 90, 40);
+
+                //======== menuBar1 ========
+                {
+
+                    //======== menu1 ========
+                    {
+                        menu1.setText("Theme >");
+                        menu1.setBorder(new MatteBorder(1, 1, 1, 1, Color.black));
+
+                        //---- darkTheme ----
+                        darkTheme.setText("Dark");
+                        darkTheme.setSelected(true);
+                        darkTheme.addActionListener(e -> darkTheme());
+                        menu1.add(darkTheme);
+
+                        //---- lightTheme ----
+                        lightTheme.setText("Light");
+                        lightTheme.addActionListener(e -> lightTheme());
+                        menu1.add(lightTheme);
+                    }
+                    menuBar1.add(menu1);
+                }
+                controlPanel.add(menuBar1);
+                menuBar1.setBounds(305, 60, 70, 30);
 
                 {
                     // compute preferred size
@@ -260,7 +403,53 @@ public class Application extends JFrame {
                 }
             }
             panel1.add(controlPanel);
-            controlPanel.setBounds(500, 5, 411, 475);
+            controlPanel.setBounds(485, 5, 395, 485);
+
+            //======== resultPanel ========
+            {
+                resultPanel.setLayout(null);
+
+                //---- label7 ----
+                label7.setText("Iteration:");
+                label7.setFont(new Font("SimSun", Font.PLAIN, 18));
+                resultPanel.add(label7);
+                label7.setBounds(20, 20, 105, 30);
+
+                //---- iterationField ----
+                iterationField.setEnabled(false);
+                iterationField.setDisabledTextColor(new Color(0x333333));
+                resultPanel.add(iterationField);
+                iterationField.setBounds(125, 20, 55, 30);
+
+                //---- label8 ----
+                label8.setText("Temperature:");
+                label8.setFont(new Font("SimSun", Font.PLAIN, 18));
+                resultPanel.add(label8);
+                label8.setBounds(215, 20, 120, 30);
+
+                //---- temperatureField ----
+                temperatureField.setEnabled(false);
+                temperatureField.setDisabledTextColor(new Color(0x333333));
+                resultPanel.add(temperatureField);
+                temperatureField.setBounds(340, 20, 100, 30);
+
+                {
+                    // compute preferred size
+                    Dimension preferredSize = new Dimension();
+                    for(int i = 0; i < resultPanel.getComponentCount(); i++) {
+                        Rectangle bounds = resultPanel.getComponent(i).getBounds();
+                        preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                        preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+                    }
+                    Insets insets = resultPanel.getInsets();
+                    preferredSize.width += insets.right;
+                    preferredSize.height += insets.bottom;
+                    resultPanel.setMinimumSize(preferredSize);
+                    resultPanel.setPreferredSize(preferredSize);
+                }
+            }
+            panel1.add(resultPanel);
+            resultPanel.setBounds(10, 420, 465, 70);
 
             {
                 // compute preferred size
@@ -286,9 +475,9 @@ public class Application extends JFrame {
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
-                .addComponent(panel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panel1, GroupLayout.DEFAULT_SIZE, 499, Short.MAX_VALUE)
         );
-        setSize(932, 532);
+        setSize(894, 537);
         setLocationRelativeTo(null);
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
@@ -313,5 +502,15 @@ public class Application extends JFrame {
     private JButton initializeButton;
     private JButton runButton;
     private JLabel messageLabel;
+    private JButton clearButton;
+    private JMenuBar menuBar1;
+    private JMenu menu1;
+    private JMenuItem darkTheme;
+    private JMenuItem lightTheme;
+    private JPanel resultPanel;
+    private JLabel label7;
+    private JTextField iterationField;
+    private JLabel label8;
+    private JTextField temperatureField;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
--- src/nqueen/Main.java
+++ src/nqueen/Main.java
@@ -1,4 +1,8 @@
 package nqueen;
+
+/**
+ * Main Application Driver
+ */
 public class Main {
     public static void main(String[] args) {
 
